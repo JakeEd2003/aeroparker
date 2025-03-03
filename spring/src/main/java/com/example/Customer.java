22f2fb89e7132475123cfe56092fe1e0380f6342
Jake Edwardson <edward72@lancaster.ac.uk>
2025-03-03 17:43:27 +0000

    defining the customer entity. Getting to grips with Spring. This part was simpler as it was pretty self explanitory

---
+++ spring/src/main/java/com/example/Customer.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //generated value auto increments for primary key value
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime registered = LocalDateTime.now(); //stores the time when someone registers and can't be updated once set
+
+    @Email
+    @NotBlank
+    @Column(name = "email_address", unique = true, nullable = false) //ensures email isn't blank and is unique
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false, length = 5)
+    @Size(max = 5) //max used as validation
+    private String title;
+
+    //first and last name are the same length with the same constraints so can be included together
+    @NotBlank
+    @Column(nullable = false, length = 50)
+    @Size(max = 50)
+    private String firstName, secondName;
+
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    @Size(max = 255)
+    private String addressLine1, addressLine2;
+
+    private String city; //city is an optional input
+
+    @NotBlank
+    @Size(max = 10)
+    private String postcode;
+
+    @Size(max = 20)
+    private String phoneNumber;
+
+}
